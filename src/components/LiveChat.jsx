ea5ded995fd12ab760fbb18156968d86ca56aca1
Arjun <arjun346002@gmail.com>
2024-01-24 15:11:20 +0530

    live chat

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
 import { generateRandomName, generateRandomString } from "../utils/helper";
 
 const LiveChat = () => {
+  const [liveChat, setLiveChat] = useState();
   const dispatch = useDispatch();
   const chatMessage = useSelector((store) => store.chat.messages);
 
@@ -22,11 +23,35 @@ const LiveChat = () => {
   }, []);
 
   return (
-    <div className="ml-4 p-2 shadow-lg bg-gray-100 rounded-lg border border-gray-500 h-[500px] overflow-y-scroll flex flex-col-reverse ">
-      {chatMessage.map((c, i) => (
-        <ChatMessage key={i} name={c.name} message={c.message} />
-      ))}
-    </div>
+    <>
+      <div>
+        <div className="ml-4 p-2 shadow-lg bg-gray-100 rounded-lg border border-gray-500 h-[500px] overflow-y-scroll flex flex-col-reverse ">
+          {chatMessage.map((c, i) => (
+            <ChatMessage key={i} name={c.name} message={c.message} />
+          ))}
+        </div>
+      </div>
+      <form
+        className=" mx-4 my-2"
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(addMessage({
+            name : "Arjun",
+            message : liveChat
+          }));
+          setLiveChat("");
+        }}
+      >
+        <input
+          className="border border-gray-900 rounded-md outline-none p-2 w-80"
+          type="text"
+          placeholder="Chat..."
+          value={liveChat}
+          onChange={(e) => setLiveChat(e.target.value)}
+        />
+        <button className="py-2 mx-2 w-16 bg-slate-400 rounded-md">Send</button>
+      </form>
+    </>
   );
 };
 
